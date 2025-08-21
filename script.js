be21ab31c126087f33e9f6f50acacbea4808ed0c
artainmo <artainmo@users.noreply.github.com>
2025-08-21 01:18:55 +0000

    deploy: 7f838cc996797b1b977c3676a4a4514dda2ae697

--- script.js
+++ script.js
@@ -329,6 +329,7 @@ https://www.youtube.com/watch?v=I-F6hzrBBS0
 https://www.youtube.com/watch?v=CeRDt9ji7jk
 https://www.youtube.com/watch?v=hdnGfzzk18k
 https://www.youtube.com/watch?v=3-NTv0CdFCk
+https://www.youtube.com/watch?v=VQPHbijKt24
 https://www.youtube.com/watch?v=nNtAgN3Vvb4
 https://www.youtube.com/shorts/WgnmcXNF87g
 https://www.youtube.com/shorts/oga0HlbGv5U
